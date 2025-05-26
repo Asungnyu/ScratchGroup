dba17505fd365ba9ed956683b55b36ce214c8e6e
Asungnyu <afavorngangeh@gmail.com>
2025-05-26 22:43:37 +0100

    daisyui setup + siteicon

--- scratchgroupcm/src/App.jsx
+++ scratchgroupcm/src/App.jsx
@@ -7,8 +7,19 @@ function App() {
   return (
     <>
       <h1 class="text-2xl text-gray-700 font-bold">Scratch Group web</h1>
+      <a role="button" className="btn">
+        Link
+      </a>
+      <button type="submit" className="btn">
+        Button
+      </button>
+      <input type="button" value="Input" className="btn" />
+      <input type="submit" value="Submit" className="btn" />
+      <input type="radio" aria-label="Radio" className="btn" />
+      <input type="checkbox" aria-label="Checkbox" className="btn" />
+      <input type="reset" value="Reset" className="btn" />
     </>
-  )
+  );
 }
 
 export default App
